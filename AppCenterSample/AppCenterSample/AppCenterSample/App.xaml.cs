b74e504a933ffa579cab11b1b9fc0d6faea6dbed
takumi-sato-git <57399218+takumi-sato-git@users.noreply.github.com>
2020-05-15 14:05:57 +0900

    Update App.xaml.cs

--- AppCenterSample/AppCenterSample/AppCenterSample/App.xaml.cs
+++ AppCenterSample/AppCenterSample/AppCenterSample/App.xaml.cs
@@ -14,6 +14,7 @@ namespace AppCenterSample
         const string uwpKey = "2b977979-ebbc-40f8-b6d2-cddb98312ac1";
         const string iosKey = "165abd2a-64d3-4371-ba02-d3cc5a1f9d61";
         const string androidKey = "ab0d38a2-dd51-4c8d-bc77-1b154b628441";
+        // hoge
 
         public App()
         {
